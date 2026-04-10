12eef2563d956c2ddb9378aaff2b341e581ba284
Suwaphat Lamaisakun <mrpondl133@gmail.com>
2026-04-10 20:29:01 +0700

    chores(api): make main() load .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 // TODO: import package สำหรับ Database (เช่น hive_flutter หรือ shared_preferences)
 
-void main(){
+void main() async{
+  await dotenv.load(fileName: ".env");
   runApp(const MaterialApp(
     home: HomeScreen()
   ));
