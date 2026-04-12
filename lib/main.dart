a6d42ccaf8571e511d70168edf1816ae59b556cb
Suwaphat Lamaisakun <mrpondl133@gmail.com>
2026-04-12 18:22:57 +0700

    chore(database): add dependencies for hive ce database

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-// TODO: import package สำหรับ Database (เช่น hive_flutter หรือ shared_preferences)
+import 'package:hive_ce_flutter/hive_ce_flutter.dart';
 
 void main() async{
+  //ตรวจสอบว่า Binding ของ Flutter พร้อมทำงาน
+  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
+
+  await Hive.initFlutter();
+  await Hive.openBox('fav_noted_Box');
+  
   runApp(const MaterialApp(
     home: HomeScreen()
   ));
